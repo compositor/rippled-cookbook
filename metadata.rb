3a5a56bde13e9d81e9c60805d2a89b6d361dff6b
compositor <d.grigorenko@snapswap.vc>
2015-09-06 22:43:25 -0700

    fix was issue

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ version '0.1.0'
 license          'Apache v2.0'
 description      'Compiles, installs and configures rippled, a ripple network daemon'
 supports         'ubuntu', '>= 14.04'
-source_url 'https://github.com/compositor/rippled-cookbook'
+# causes AWS to fail source_url 'https://github.com/compositor/rippled-cookbook'
 depends 'apt'
 issues_url 'https://github.com/compositor/rippled-cookbook/issues'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
